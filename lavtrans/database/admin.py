f32ce8d718781370781c12d540638254c46e79da
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-06-26 12:36:11 +0300

    add InsuranceEvent and ImagesInsuranceEvent models

--- lavtrans/database/admin.py
+++ lavtrans/database/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
-from .models import Car, Driver
+from .models import Car, Driver, InsuranceEvent, ImagesInsuranceEvent
+
 
 # Register your models here.
+class InsuranceEventInline(admin.TabularInline):
+    model = InsuranceEvent
+    extra = 0
+
+
+class ImagesInsuranceEventInline(admin.TabularInline):
+    model = ImagesInsuranceEvent
+    extra = 0
+
+
+class ImagesInsuranceEventAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ImagesInsuranceEvent._meta.fields]
+
+
+class InsuranceEventAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in InsuranceEvent._meta.fields]
+    inlines = [ImagesInsuranceEventInline]
+
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Driver._meta.fields]
+    inlines = [InsuranceEventInline]
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Car._meta.fields]
+    inlines = [InsuranceEventInline]
+
+
 admin.site.register(Car)
 admin.site.register(Driver)
+admin.site.register(InsuranceEvent)
+admin.site.register(ImagesInsuranceEvent)
--- lavtrans/database/models.py
+++ lavtrans/database/models.py
@@ -36,3 +36,53 @@ class Driver(models.Model):
     class Meta:
         verbose_name = 'Водитель'
         verbose_name_plural = 'Водители'
+
+
+class InsuranceEvent(models.Model):
+    CALCULATION = 'Калькуляция'
+    SERVICE = 'Сервис'
+
+    METHOD = [
+        (CALCULATION, 'Калькуляция'),
+        (SERVICE, 'Сервис'),
+    ]
+
+    car = models.ForeignKey('Car', on_delete=models.CASCADE)
+    driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
+    date_of_submission = models.DateField(verbose_name='Дата подачи в страховую')
+    polis_number = models.CharField(max_length=10, verbose_name='Страховой полис')
+    police_sertificate = models.BooleanField(verbose_name='Справка с ГАИ?', default=False)
+    repair_method = models.CharField(max_length=13, choices=METHOD, verbose_name='Способ ремонта', blank=True,
+                                     null=True)
+    calculation_sum = models.DecimalField(max_digits=20, decimal_places=2,
+                                          verbose_name='Страховая насчитала по калькуляции',
+                                          blank=True, null=True)
+    expenses = models.DecimalField(max_digits=20, decimal_places=2,
+                                   verbose_name='Наши расходы по восстановлению (Калькуляция)', blank=True, null=True)
+    margin = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='В нашу пользу (Калькуляция)',
+                                 blank=True, null=True)
+    service_name = models.CharField(max_length=50, verbose_name='На какой сервис отправили ТС', blank=True, null=True)
+    service_date = models.DateField(verbose_name='Дата отправки на сервис', blank=True, null=True)
+    service_sum = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Сумма счета от сервиса',
+                                      blank=True, null=True)
+    final_docs = models.DateField(verbose_name='Дата передачи документов по ремонту в страховую', blank=True, null=True)
+    payment_date = models.DateField(verbose_name='Дата оплаты от страховой', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.car} {self.driver}'
+
+    class Meta:
+        verbose_name = 'Страховой случай'
+        verbose_name_plural = 'Страховые случаи'
+
+
+class ImagesInsuranceEvent(models.Model):
+    insurance_event = models.ForeignKey('InsuranceEvent', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='database/images', blank=True, null=True, verbose_name='Фото')
+
+    def __str__(self):
+        return f'{self.insurance_event}'
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
