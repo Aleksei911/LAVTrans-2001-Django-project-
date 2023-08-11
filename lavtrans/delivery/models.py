3ecf1971190da40f99596c72ccf11a090cc819d0
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-08-11 15:48:02 +0300

    update models and admin for delivery

--- lavtrans/delivery/models.py
+++ lavtrans/delivery/models.py
@@ -29,18 +29,20 @@ class Delivery(models.Model):
     ]
 
     date_of_delivery = models.DateField(verbose_name='Дата загрузки')
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_delivery_set')
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='driver_delivery_set')
     transporter = models.CharField(max_length=25, choices=TRANSPORTER, verbose_name='Перевозчик')
-    next_car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True)
-    next_driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=True, blank=True)
+    next_car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True,
+                                 related_name='next_car_delivery_set')
+    next_driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=True, blank=True,
+                                    related_name='next_driver_delivery_set')
     next_transporter = models.CharField(max_length=25, choices=TRANSPORTER, verbose_name='Последующий перевозчик',
                                         blank=True, null=True)
     waybill_number = models.CharField(max_length=5, verbose_name='Номер путевого листа', null=True, blank=True)
-    waybill_date = models.DateField(verbose_name='Дата путевого листа')
+    waybill_date = models.DateField(verbose_name='Дата путевого листа', null=True, blank=True)
     customer = models.CharField(max_length=50, verbose_name='Заказчик', null=True, blank=True)
     application_number = models.CharField(max_length=15, verbose_name='Номер заявки', null=True, blank=True)
-    application_date = models.DateField(verbose_name='Дата заявки')
+    application_date = models.DateField(verbose_name='Дата заявки', null=True, blank=True)
     route = models.CharField(max_length=255, verbose_name='МАРШРУТ (ЗАГРУЗКА -ТАМОЖНИ +ДОВОЗ)', null=True, blank=True)
     rate = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Ставка', blank=True, null=True)
     rate_currency = models.CharField(max_length=4, choices=CURRENCY, verbose_name='Денежная единица для ставки',
@@ -57,5 +59,12 @@ class Delivery(models.Model):
                                           verbose_name='Стоимость электронной пломбы (BYN)')
     prostoi = models.IntegerField(verbose_name='Количество дней простоя', blank=True, null=True)
     score_number = models.CharField(max_length=10, blank=True, null=True, verbose_name='Номер счёта')
-    score_date = models.DateField(verbose_name='Дата выставления счёта')
+    score_date = models.DateField(verbose_name='Дата выставления счёта', null=True, blank=True)
     payment_term = models.IntegerField(verbose_name="Срок оплаты (Банковских дней)", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.date_of_delivery} - {self.car}"
+
+    class Meta:
+        verbose_name = 'Перевозка'
+        verbose_name_plural = 'Перевозки'
