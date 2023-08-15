6686b1fbc6775e50e372892d97d1037e33463a34
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-08-15 22:55:11 +0300

    update urls, model delivery and delivery templates

--- lavtrans/delivery/urls.py
+++ lavtrans/delivery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', all_deliveries, name='delivery'),
     path('add_delivery/', add_delivery, name='add_delivery'),
     path('delivery_info/<int:pk>', delivery_info, name='delivery_info'),
+    path('delivery_edit/<int:pk>', delivery_edit, name='delivery_edit'),
 ]
